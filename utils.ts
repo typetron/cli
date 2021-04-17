81c4f6c325e9c477b8a0106273d35099a1571347
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2021-04-18 00:07:11 +0300

    Typetron CLI now load the env configs correctly

--- utils.ts
+++ utils.ts
@@ -7,7 +7,7 @@ export function prepareApp() {
     const tsConfigPaths = require('tsconfig-paths')
 
     // TODO this package tries to load a ton of files until it finds the right one
-    //  Need more investigation on this as it slows down the app quite a bit
+    //  Need more investigation on this as it slows down the CLI app quite a bit
     tsConfigPaths.register({
         baseUrl: './',
         mainFields: ['index'],
@@ -18,6 +18,10 @@ export function prepareApp() {
 export async function boostrapApp() {
     prepareApp()
 
+    const dotenv = require(path.join(process.cwd(), 'node_modules', 'dotenv'))
+    dotenv.config({path: path.join(process.cwd(), '.env')})
+
     const {appBuilder} = require(path.join(process.cwd(), 'app.ts')) as {appBuilder: Promise<Application>}
+
     return await appBuilder
 }
